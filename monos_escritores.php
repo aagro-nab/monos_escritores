d71e4192aa2bb5bc8be82d4dd15dc21c04e8b489
Ame <322317150@alumno.enp.unam.mx>
2023-05-20 14:17:16 -0600

    Cambios a monos_escritores.php

--- monos_escritores.php
+++ monos_escritores.php
@@ -1,22 +1,36 @@
-<?php
-    //-----------------------> FECHA Y HORA <-----------------------//
-    
-    date_default_timezone_set("Asia/Choibalsan");
-    $zona_horaria = date_default_timezone_get();
-    $ahora = date('h:i:s a');
-    $fecha = date('d/m/Y');
-    
-    $fecha_rand = array(
-        "hour" => rand(1, 12), 
-        "minute" => rand(1, 59),
-        "second" => rand(1, 59),
-        "mes" => rand(1, 12),
-        "dia" => rand(1, 31),
-        "año" => rand(1000, 2023));
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        
+        
+        //-----------------------> FECHA Y HORA <-----------------------//
+        
+        date_default_timezone_set("Asia/Choibalsan");
+        $zona_horaria = date_default_timezone_get();
+        $ahora = date('h:i:s a');
+        $fecha = date('d/m/Y');
+        
+        $fecha_rand = array(
+            "hour" => rand(1, 12), 
+            "minute" => rand(1, 59),
+            "second" => rand(1, 59),
+            "mes" => rand(1, 12),
+            "dia" => rand(1, 31),
+            "año" => rand(1000, 2023));
 
-    $salida = mktime($fecha_rand["hour"], $fecha_rand["minute"], $fecha_rand["second"], $fecha_rand["mes"], $fecha_rand["dia"], $fecha_rand["año"]);
+        $salida = mktime($fecha_rand["hour"], $fecha_rand["minute"], $fecha_rand["second"], $fecha_rand["mes"], $fecha_rand["dia"], $fecha_rand["año"]);
 
-    echo "La fecha de consulta de este libro fue el ".$fecha." a las ".$ahora." en ".$zona_horaria;
-    echo '<br><br>';
-    echo "El libro se escribió el ".date("d/m/Y", $salida).'<br>';
-?>
+        echo "La fecha de consulta de este libro fue el ".$fecha." a las ".$ahora." en ".$zona_horaria;
+        echo '<br><br>';
+        echo "El libro se escribió el ".date("d/m/Y", $salida).'<br>';
+    ?>
+
+</body>
+</html>
